ae634616ae63fafa9f46b31a2ac0b1297d15fe5f
Sanjeev Sharma <sanjeev.sharma@acefone.com>
2025-03-02 20:29:27 +0530

    feat: open telemetry init

--- src/middlewares/trace.middleware.ts
+++ src/middlewares/trace.middleware.ts
@@ -1,10 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import { v4 as uuidv4 } from 'uuid';
-import { asyncLocalStorage, TraceIdHandler } from "../utils";
+import { asyncLocalStorage, TraceIdHandler, tracer } from "../utils";
 
 export function traceMiddleware(
   req: Request,
-  _: Response,
+  res: Response,
   next: NextFunction
 ) {
   const traceId =
@@ -12,5 +12,15 @@ export function traceMiddleware(
     (req?.body?.[TraceIdHandler.getTraceIdField()] as string) ??
     (req?.query?.[TraceIdHandler.getTraceIdField()] as string) ??
     uuidv4();
-  asyncLocalStorage.run({ traceId }, () => next());
+  asyncLocalStorage.run({ traceId }, () => {
+    if (true) {
+      tracer.startActiveSpan("request-span", (span) => {
+        span.setAttribute("traceId", traceId);
+        res.on("finish", () => span.end());
+        next();
+      });
+    } else {
+      next();
+    }
+    });
 }
--- src/utils/trace.util.ts
+++ src/utils/trace.util.ts
@@ -1,4 +1,10 @@
-import { TRACE_ID } from "../constants";
+import { APP_NAME, TRACE_ID } from "../constants";
+import { NodeTracerProvider } from "@opentelemetry/sdk-trace-node";
+import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-base";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { TraceIdRatioBasedSampler } from "@opentelemetry/sdk-trace-base";
+import { trace } from "@opentelemetry/api";
+import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-base";
 
 export class TraceIdHandler {
   private static traceId: string = TRACE_ID;
@@ -11,3 +17,17 @@ export class TraceIdHandler {
     TraceIdHandler.traceId = traceId;
   }
 }
+
+
+
+const provider = new NodeTracerProvider({
+  sampler: new TraceIdRatioBasedSampler(1.0),
+  spanProcessors: [
+    new BatchSpanProcessor(new ConsoleSpanExporter()),
+    new BatchSpanProcessor(new OTLPTraceExporter({ url: "http://localhost:4317/v1/traces" })),
+  ],
+});
+
+provider.register();
+
+export const tracer = trace.getTracer(APP_NAME);
